bed2e927bca93756cae307703977b05dac165623
KhorneDisciple <amplifier.prince@hotmail.com>
2015-12-16 00:58:32 +0000

    Create cRocket.cpp

---
+++ cRocket.cpp
@@ -0,0 +1,162 @@
+/*
+=================
+cRocket.cpp
+- Header file for class definition - IMPLEMENTATION
+=================
+*/
+#include "cRocket.h"
+#include "cSprite.h"
+#include "asteroidsGame.h"
+
+int score = 0;
+
+void cRocket::render()
+{
+		
+		glPushMatrix();
+
+		glTranslatef(spritePos2D.x, 768, 0.0f);
+		glRotatef(spriteRotation, 0.0f, 0.0f, 1.0f);
+		glScalef(spriteScaling.x, spriteScaling.y, 1.0f);
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, GLTextureID); // Binding of GLtexture name 
+
+		glBegin(GL_QUADS);
+		glColor3f(255.0f, 255.0f, 255.0f);
+		glTexCoord2f(spriteTexCoordData[0].x, spriteTexCoordData[0].y);
+		glVertex2f(-(textureWidth / 2), -(textureHeight / 2));
+		glTexCoord2f(spriteTexCoordData[1].x, spriteTexCoordData[1].y);
+		glVertex2f((textureWidth / 2), -(textureHeight / 2));
+		glTexCoord2f(spriteTexCoordData[2].x, spriteTexCoordData[2].y);
+		glVertex2f((textureWidth / 2), (textureHeight / 2));
+		glTexCoord2f(spriteTexCoordData[3].x, spriteTexCoordData[3].y);
+		glVertex2f(-(textureWidth / 2), (textureHeight / 2));
+
+		glEnd();
+		glDisable(GL_TEXTURE_2D);
+
+		glPopMatrix();
+	
+}
+/*
+=================================================================
+Update the sprite position
+=================================================================
+*/
+
+void cRocket::update(float deltaTime)
+{
+	
+		if (m_InputMgr->isKeyDown(VK_RIGHT))
+		{
+			spritePos2D.x += 30;
+		}
+		if (m_InputMgr->isKeyDown(VK_LEFT))
+		{
+			spritePos2D.x -= 30;
+		}
+
+		if (m_InputMgr->isKeyDown(VK_SPACE))
+		{
+			theRocketBullets.push_back(new cBullet);
+			int numBullets = theRocketBullets.size() - 1;
+			theRocketBullets[numBullets]->setSpritePos(glm::vec2(spritePos2D.x + spriteCentre.x, 768));
+			theRocketBullets[numBullets]->setSpriteTranslation(glm::vec2(2.0f, 2.0f));
+			theRocketBullets[numBullets]->setTexture(theGameTextures[4]->getTexture());
+			theRocketBullets[numBullets]->setTextureDimensions(theGameTextures[4]->getTWidth()*0.8, theGameTextures[4]->getTHeight() / 2);
+			theRocketBullets[numBullets]->setSpriteCentre();
+			theRocketBullets[numBullets]->setBulletVelocity(glm::vec2(0.0f, 0.0f));
+			theRocketBullets[numBullets]->setSpriteRotation(getSpriteRotation());
+			theRocketBullets[numBullets]->setActive(true);
+			theRocketBullets[numBullets]->setMdlRadius();
+
+		}
+
+		if (spriteRotation > 360)
+		{
+			spriteRotation -= 360.0f;
+		}
+
+		glm::vec2 spriteVelocityAdd = glm::vec2(0.0f, 0.0f);
+		spriteVelocityAdd.x = (glm::sin(glm::radians(spriteRotation)));
+		spriteVelocityAdd.y = -(glm::cos(glm::radians(spriteRotation)));
+
+		spriteVelocityAdd *= spriteTranslation;
+
+		rocketVelocity += spriteVelocityAdd;
+
+		spritePos2D += rocketVelocity * deltaTime;
+
+		rocketVelocity *= 0.95;
+
+		/*
+		==============================================================
+		| Check for collisions
+		==============================================================
+		*/
+		for (vector<cBullet*>::iterator index = theRocketBullets.begin(); index != theRocketBullets.end(); ++index)
+		{
+			for (vector<cAsteroid*>::iterator iter = theAsteroids.begin(); iter != theAsteroids.end(); ++iter)
+			{
+				if ((*iter)->collidedWith((*iter)->getBoundingRect(), (*index)->getBoundingRect()))
+				{
+					// if a collision set the bullet and asteroid to false
+					(*index)->setActive(false);
+					(*iter)->setActive(false);
+					counter = counter + 0.5;
+					astro--;
+					score++;
+
+				}
+
+			}
+		}
+		if (astro < 1)
+		{
+			theAsteroids.push_back(new cAsteroid);
+			theAsteroids[astro]->setSpritePos(glm::vec2(1024 / (rand() % 5 + 1), 0));
+			theAsteroids[astro]->setSpriteTranslation(glm::vec2(0, 20 * counter));
+			theAsteroids[astro]->setTexture(theGameTextures[rand() % 4]->getTexture());
+			theAsteroids[astro]->setTextureDimensions(theGameTextures[rand() % 1]->getTWidth() / 2, theGameTextures[rand() % 1]->getTHeight() / 2);
+			theAsteroids[astro]->setSpriteCentre();
+			theAsteroids[astro]->setAsteroidVelocity(glm::vec2(glm::vec2(0.0f, 0.0f)));
+			theAsteroids[astro]->setActive(true);
+			theAsteroids[astro]->setMdlRadius();
+			astro++;
+
+		}
+		vector<cBullet*>::iterator index = theRocketBullets.begin();
+		while (index != theRocketBullets.end())
+		{
+			if ((*index)->isActive() == false)
+			{
+				index = theRocketBullets.erase(index);
+			}
+			else
+			{
+				(*index)->update(deltaTime);
+				(*index)->render();
+				++index;
+			}
+		}
+	
+}
+/*
+=================================================================
+  Sets the velocity for the rocket
+=================================================================
+*/
+void cRocket::setRocketVelocity(glm::vec2 rocketVel)
+{
+	rocketVelocity = rocketVel;
+}
+/*
+=================================================================
+  Gets the rocket velocity
+=================================================================
+*/
+glm::vec2 cRocket::getRocketVelocity()
+{
+	return rocketVelocity;
+}
